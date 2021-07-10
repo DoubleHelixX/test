e96375cc947e1381ea5132a010bb4eec2645aaad
DoubleHelix <tjrnyc5@gmail.com>
2021-07-10 15:24:07 -0400

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>GITHUB PAGES TEST DUE TO BUG</p>
+        <p>GITHUB PAGES TEST DUE TO BUG 😳</p>
         <a
           className="App-link"
           href="https://reactjs.org"
